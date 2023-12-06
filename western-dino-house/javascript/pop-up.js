e976adaf9daec65352f0d3803cc1018af3e65bdf
Matthew Sullivan <143848513+Daceit@users.noreply.github.com>
2023-12-06 14:47:45 -0800

    aaa

---
+++ western-dino-house/javascript/pop-up.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("buyPopUp").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("buyPopUp").style.display = "none";
+  }
